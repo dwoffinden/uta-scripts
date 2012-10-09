1136d101f351dfc60dcd8ed12b12f35a858b1b27
Daniel Woffinden <daw10@doc.ic.ac.uk>
2012-10-09 17:28:35 +0100

    Initial commit.

---
+++ uta.pl
@@ -0,0 +1,66 @@
+#!/usr/bin/perl -w
+
+use strict;
+use feature 'switch';
+
+my $exercise;
+
+sub usage {
+  print <<END;
+usage:
+  $0 init EXERCISE
+    create git repo EXERCISE, fetching all tutee branches
+
+  $0 fetch
+    fetch from all remotes for the current repo
+END
+}
+
+sub init {
+  my $exercise;
+  if ($_[0]) {
+    $exercise = $_[0];
+  } else {
+    die;
+  }
+
+  open(SSH, 'ssh -p 10022 labranch.doc.ic.ac.uk |');
+
+  system("git init $exercise");
+
+  chdir "$exercise";
+
+  while (my $line = <SSH>) {
+    if ($line =~ m|git clone (ssh://.+?/lab/(.+?)/.+?/$exercise)|) {
+      my $url = $1;
+      my $tutee = $2;
+      system("git remote add --fetch $tutee $url");
+    };
+  }
+
+  system("git gc")
+}
+
+sub fetch {
+  open(SSH, 'git remote |');
+
+  while (my $remote = <SSH>) {
+    system("git fetch $remote");
+  }
+}
+
+given ($ARGV[0]) {
+  when ("init") {
+    if (defined($ARGV[1])) {
+      init $ARGV[1];
+    } else {
+      usage; exit;
+    }
+  }
+  when ("fetch") {
+    fetch;
+  }
+  default {
+    usage; exit;
+  }
+}
